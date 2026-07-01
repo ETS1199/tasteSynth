4583585643bb788fc13ce97087142f7402c4941f
GalacticRe <dwlow@ualberta.ca>
2026-07-01 16:39:57 -0600

    Created merge and playlist view

--- project/tasteSynth/urls.py
+++ project/tasteSynth/urls.py
@@ -5,5 +5,7 @@ urlpatterns = [
     path("",views.SynthView, name="synth_view"),
     path("authorization",views.SpotifyAuthorization, name="spotify_authorization"),
     path("auth_success",views.SuccessfulAuthorization, name="successful_authorization"),
-    path("playlist_display",views.PlaylistDisplay, name="playlist_display")
+    path("playlist_display",views.PlaylistDisplay, name="playlist_display"),
+    path("playlist_view/<str:id>",views.PlaylistView, name="playlist_view"),
+    path("merge_view",views.MergeView, name="merge_view")
 ]
